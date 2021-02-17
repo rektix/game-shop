4f0c20d7e70b1a96092b5df1ad2af65d8995e6b8
MihajloKrsmanovic <mihajlo@dataandmore.com>
2021-02-17 10:35:45 +0100

    Query fixes

--- django_analytics/game_shop/main/views.py
+++ django_analytics/game_shop/main/views.py
@@ -12,11 +12,11 @@ def home(request):
     games_per_genre = Game.objects.values(fieldname).order_by(fieldname).annotate(count=Count(fieldname))
     games_per_genre = [[x['genre'], x['count']] for x in games_per_genre]
 
-    per_day = Purchase.objects.extra(select={'day': 'date( date )'}).values('day').annotate(day_sum=Sum('game__price'))
+    per_day = Purchase.objects.extra(select={'day': 'date( date )'}).values('day').annotate(day_sum=Sum('game__price')).order_by('day')
     per_day = [[x['day'].strftime('%d.%m.%Y'), x['day_sum']] for x in per_day]
     per_day.insert(0, ['Date', 'Sales in $'])
 
-    top_5 = Purchase.objects.values('game__name').annotate(count=Count('game')).order_by('count')[:5]
+    top_5 = Purchase.objects.values('game__name').annotate(count=Count('game')).order_by('-count')[:5]
     print(top_5)
 
     return render(request, 'main/index.html', { 'gamesPerGenre': json.dumps(games_per_genre), 'moneyPerDay': json.dumps(per_day), 'topGames': top_5 })
